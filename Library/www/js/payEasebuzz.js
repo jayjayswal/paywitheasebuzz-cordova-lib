17f00fbb846a42771f46f56f5ea10ea6c9f73b61
jay <jay@localhost.localdomain>
2016-11-23 17:41:51 +0530

    jay: lib logs deleted

--- Library/www/js/payEasebuzz.js
+++ Library/www/js/payEasebuzz.js
@@ -2,13 +2,13 @@ var easebuzz = {
     easebuzzCheckout: function (params) {
         baseUrl = "https://pay.easebuzz.in/";
         console.log("In calling " + baseUrl + "cordova/initiateLink");
-        str = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10"
+        str = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
         var hashparam = str.split("|");
         var hashstr = "";
         for (i = 0; i < hashparam.length; i++) {
             hashstr += params[hashparam[i]] + "|";
         }
-        hashstr += params["salt"] + "|" + params["key"]
+        hashstr += params["salt"] + "|" + params["key"];
         var shaObj = new jsSHA("SHA-512", "TEXT");
         shaObj.update(hashstr);
         var paymenthash = shaObj.getHash("HEX");
@@ -33,13 +33,12 @@ var easebuzz = {
                 udf4: params.udf4,
                 udf5: params.udf5,
                 is_coupon_enabled: 1,
-                isMobile: 2,
+                isMobile: 2
             }
         });
         request.done(function (response) {
-            var EBresponse = "NA"
+            var EBresponse = "NA";
             var nameInterval;
-            console.log("response" + response);
             console.log(JSON.stringify(response, null, "    "));
             var windowUrl = "";
             if (response.status == 1) {
@@ -57,17 +56,17 @@ var easebuzz = {
                         nameInterval = null;
                         if (windowUrl == baseUrl + "cordova/response_url") {
                             nameInterval = setInterval(function () {
-                                console.log("==================IN Response==============================")
+                                console.log("==================IN Response==============================");
                                 inappEasebuzz.executeScript({code: "localStorage.getItem('easebuzzResponse')"}, function (values) {
-                                    console.log("==================Got Local Storage=========================")
+                                    console.log("==================Got Local Storage=========================");
                                     EBresponse = values[0];
                                     if (EBresponse != "NA") {
                                         var obj = JSON.parse(EBresponse);
                                         EBresponse = obj;
                                         console.log(EBresponse);
                                         if (obj.hasOwnProperty("flag")) {
-                                            console.log("==================Got Response=========================")
-                                            console.log("================== CLEAR =========================")
+                                            console.log("==================Got Response=========================");
+                                            console.log("================== CLEAR =========================");
                                             inappEasebuzz.executeScript({code: "localStorage.setItem('easebuzzResponse', '')"});
                                             clearInterval(nameInterval);
                                             inappEasebuzz.close();
@@ -80,16 +79,15 @@ var easebuzz = {
                     });
                 });
                 inappEasebuzz.addEventListener('exit', function () {
-                    app.showResponse();
                     if (EBresponse == "NA") {
-                        document.getElementById('txnStatus').innerHTML = "Cancelled transaction";
+                        alert("Cancelled transaction");
                     }
                     
 
                     if (EBresponse.hasOwnProperty("flag")) {
                         if (EBresponse['flag'] == 0) {
                             if (EBresponse['status'] == "success") {
-                                var str = "status|udf10|udf9|udf8|udf7|udf6|udf5|udf4|udf3|udf2|udf1|email|firstname|productinfo|amount|txnid"
+                                var str = "status|udf10|udf9|udf8|udf7|udf6|udf5|udf4|udf3|udf2|udf1|email|firstname|productinfo|amount|txnid";
                                 var hashparam = str.split("|");
                                 var hashstr = params["salt"]+"|";
                                 for (i = 0; i < hashparam.length; i++) {
@@ -100,26 +98,27 @@ var easebuzz = {
                                 shaObj.update(hashstr);
                                 var paymenthash = shaObj.getHash("HEX");
                                 if(paymenthash==EBresponse['hash']){
-                                    document.getElementById('txnStatus').innerHTML = "Successful transaction";
+                                    alert("Successful transaction");
                                 }else{
-                                    document.getElementById('txnStatus').innerHTML = "Response hash mismatch";
+                                    alert("Response hash mismatch");
                                 }
                             } else {
-                                document.getElementById('txnStatus').innerHTML = "Failed transaction";
+                                alert("Failed transaction");
                             }
                         } else {
-                            document.getElementById('txnStatus').innerHTML = EBresponse['error_status'];
+                            alert(EBresponse['error_status']);
                         }
                     } else {
-                        document.getElementById('txnStatus').innerHTML = "Cancelled/Error transaction";
+                        alert("Cancelled/Error transaction");
                     }
-                    document.getElementById('resDiv').innerHTML = JSON.stringify(EBresponse);
+                    alert(JSON.stringify(EBresponse));
+                    console.log("============= RESPONSE ==========================");
+                    console.log(JSON.stringify(EBresponse));
                 });
             } else {
-                app.hideLoading();
                 console.log(response.data);
                 console.log(response.error_desc);
-                alert(response.data+" : "+response.error_desc)
+                alert(response.data+" : "+response.error_desc);
             }
             console.log("It Opened");
         });
@@ -128,4 +127,4 @@ var easebuzz = {
             alert("Request failed: " + textStatus);
         });
     }
-}
+};
--- README.md
+++ README.md
@@ -13,4 +13,30 @@ paywitheasebuzz-cordova-lib
 
 #### Steps to integrate
 1. Meta tag Content-Security-Policy add "connect-src *;"
-2. Add jquery-1.11.1.min.js,sha512.js and import it before index.js
+2. Add jquery-1.11.1.min.js,sha512.js,payEasebuzz.js from Library folder and import it before index.js
+3. call easebuzz.easebuzzCheckout(params) function to start a payment
+param Example:
+```javascript
+params = {
+    "firstname": "John",
+    "amount": "1",
+    "email": "john@hgh.com",
+    "phone": "9158520755",
+    "productinfo": "test product",
+    "udf1": "xyz",
+    "udf2": "xyz",
+    "udf3": "xyz",
+    "udf4": "xyz",
+    "udf5": "xyz",
+    "udf6": "",
+    "udf7": "",
+    "udf8": "",
+    "udf9": "",
+    "udf10": "",
+    "surl": "https://pay.easebuzz.in/",
+    "furl": "https://pay.easebuzz.in/",
+    "txnid": "123456789",
+    "key": "XXXXXXXXX",
+    "salt": "XXXXXXXXX",
+}
+``` 
